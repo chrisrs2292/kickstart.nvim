bd28005dbb0b50950a1554e63e2ba35314180f62
chrisrs2292 <15940096+chrisrs2292@users.noreply.github.com>
2026-01-07 21:15:23 +0000

    add go support

--- init.lua
+++ init.lua
@@ -718,6 +718,17 @@ require('lazy').setup({
             },
           },
         },
+        gopls = {
+          settings = {
+            gopls = {
+              anaylses = {
+                unusedparams = true,
+              },
+              staticcheck = true,
+              gofumpt = true
+            }
+          }
+        }
       }
 
       -- Ensure the servers and tools above are installed
@@ -964,7 +975,23 @@ require('lazy').setup({
     main = 'nvim-treesitter.configs', -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     opts = {
-      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
+      ensure_installed = { 
+        'bash', 
+        'c', 
+        'diff', 
+        'html', 
+        'lua', 
+        'luadoc', 
+        'markdown', 
+        'markdown_inline', 
+        'query', 
+        'vim', 
+        'vimdoc',
+        'go',
+        'gomod',
+        'gowork',
+        'gosum'
+      },
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
